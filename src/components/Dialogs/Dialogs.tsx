ed23bda180dfb57f359c198d92eced978c4d0fa5
Maxim Predko <maxpredko@gmail.com>
2022-01-23 21:51:29 +0300

    feat: done 22th lesson

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,7 +6,7 @@ function Dialogs() {
   return (
     <main className={classes.dialogs}>
       <div className={classes.dialogsList}>
-        <NavLink to="" className={classes.dialog}>
+        <NavLink to="/dialogs/1" className={classes.dialog}>
           <img
             src="../../img/avatar.jpg"
             alt="user-avatar"
@@ -14,38 +14,38 @@ function Dialogs() {
           />
           Maxim
         </NavLink>
-        <div className={classes.dialog}>
+        <NavLink to="/dialogs/2" className={classes.dialog}>
           <img
             src="../../img/avatar.jpg"
             alt="user-avatar"
             className={classes.userAvatar}
           />
           Olga
-        </div>
-        <div className={classes.dialog}>
+        </NavLink>
+        <NavLink to="/dialogs/3" className={classes.dialog}>
           <img
             src="../../img/avatar.jpg"
             alt="user-avatar"
             className={classes.userAvatar}
           />
           Pavel
-        </div>
-        <div className={classes.dialog}>
+        </NavLink>
+        <NavLink to="/dialogs/4" className={classes.dialog}>
           <img
             src="../../img/avatar.jpg"
             alt="user-avatar"
             className={classes.userAvatar}
           />
           Karina
-        </div>
-        <div className={classes.dialog}>
+        </NavLink>
+        <NavLink to="/dialogs/5" className={classes.dialog}>
           <img
             src="../../img/avatar.jpg"
             alt="user-avatar"
             className={classes.userAvatar}
           />
           Oleg
-        </div>
+        </NavLink>
       </div>
       <div className={classes.messagesList}>
         <div className={classes.message}>Hello!</div>
