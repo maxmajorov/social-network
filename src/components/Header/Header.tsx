e30fa82d81e7f6b6d65feae15eaf43ff33ee734a
Maxim Predko <maxpredko@gmail.com>
2022-01-23 15:26:59 +0300

    refactor css files

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ function Header() {
   return (
     <header className={classes.header}>
       <img src="./img/logo.png" className={classes.logo} alt="logo" />
+      <span className={classes.title}>Social Network</span>
     </header>
   );
 }
