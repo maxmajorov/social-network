da5c1cf33123417921a130c62a1c26702dd9d88e
Maxim Predko <maxpredko@gmail.com>
2022-08-07 16:34:06 +0300

    add gh-pages & change BrowserRouter ==> HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./bll/store";
 import { App } from "./app/App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
